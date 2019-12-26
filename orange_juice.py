dfc8a87edf6a65bfed46462acfaea599096286c8
CallMeMushroom <28999497+CallMeMushroom@users.noreply.github.com>
2019-12-26 11:53:57 +0800

    update orange_juice.py

--- orange_juice.py
+++ orange_juice.py
@@ -1,58 +1,84 @@
-class Unit():
+class Unit:
 
-    def __init__(self):
-        self.HP, self.ATK, self.DEF, self.EVD = map(int, input().split())
+    def __init__(self, name):
+        self.name = name
+        while True:
+            try:
+                print(self.name, 'status (HP, ATK, DEF, EVD): ', end='')
+                self.HP, self.ATK, self.DEF, self.EVD = map(int, input().split())
+                break
+            except:
+                pass
+
 
 
 def main():
+    '''https://github.com/CallMeMushroom/100percent-Orange-Juice-Tools'''
 
-    def defensive():
+    def checked_input(prompt_str, type_func):
+        while True:
+            try:
+                print(prompt_str, end='')
+                return type_func(input())
+            except:
+                pass
 
-        print('Enemy Attack: ', end='')
-        ATK = int(input())
-        print('Your status (HP, ATK, DEF, EVD): ', end ='')
-        you = Unit()
-        
 
-        for action in 'DEF', 'EVD':
+    def defensive(ATK, defender, action):
 
-            die_count, total_damage = 0, 0
-            for roll in range(1, 7):
+        die_count, total_damage = 0, 0
+        for roll in range(1, 7):
 
-                if action == 'DEF':
-                # try to defend
-                    damage = max( 1 , ATK - (you.DEF + roll) )
-                elif action == 'EVD':
-                # try to evade
-                    damage = 0 if you.EVD + roll > ATK else ATK
+            if action == 'DEF':
+            # try to defend
+                damage = max( 1 , ATK - (defender.DEF + roll) )
+            elif action == 'EVD':
+            # try to evade
+                damage = 0 if defender.EVD + roll > ATK else ATK
                 
-                if you.HP > damage:
-                    total_damage += damage
-                else:
-                    die_count += 1
-            
-            if die_count == 6:
-                print(f'[{action}] CERTAIN DEATH.')
+            if defender.HP > damage:
+                total_damage += damage
             else:
-                print(f'[{action}] {die_count / 6 * 100:.2f}% Die;\n      {(6-die_count) / 6 * 100:.2f}% Survive, avarage damage: {total_damage / (6-die_count):.2f}.')
+                die_count += 1
+            
+        return die_count, total_damage
 
-
-    def offensive():
-
-        print('Your status (HP, ATK, DEF, EVD): ', end ='')
-        you = Unit()        
-        print('Enemy status (HP, ATK, DEF, EVD):', end='')
-        enemy = Unit()
-        # WORK IN PROGRESS
+        
+    def offensive(attacker, defender):
+        
+        print('Offensive mode is working in progress.')
+        return
+        # WORKING IN PROGRESS
 
 
     while True:
         print('\nChoose mode (Offensive(O/0) or Defensive(D/1)): ', end='')
         mode = input().lower()
+
         if mode in ('d', '1', 'defensive', 'def'):
-            defensive()
+        # Player is the defender
+            ATK = checked_input('Enemy attack: ', int)            
+            you = Unit('Your')
+
+            for action in 'DEF', 'EVD':
+
+                die_count, total_damage = defensive(ATK, you, action)
+                if die_count == 6:
+                    print(f'[{action}] CERTAIN DEATH.')
+                else:
+                    print(f'[{action}] {die_count / 6 * 100:.2f}% Die;')
+                    print(' ' * 5, f'{(6-die_count) / 6 * 100:.2f}% Survive, avarage damage: {total_damage / (6-die_count):.2f}.')
+
+
         elif mode in ('o', '0', 'offensive', 'off'):
-            offensive()
+        # Player is the offender
+            you = Unit('Your')        
+            enemy = Unit('Enemy')
+
+            offensive(you, enemy)
+            if enemy.HP > 0:
+                offensive(enemy, you)
+
         else:
             print('Nigh!')
 
