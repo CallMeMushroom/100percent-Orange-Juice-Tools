1b4841d34c8c3b13b253efd5173547833bc0cd34
CallMeMushroom <mushroom.izzet@gmail.com>
2019-12-29 14:33:23 +0800

    function completed
    All functions completed with command line only.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 100percent Orange Juice Tools
- Tools that can help wisely play 100% Orange Juice
+# 100% 鲜橙汁！
--- orange_juice.py
+++ orange_juice.py
@@ -2,9 +2,12 @@ class Unit:
 
     def __init__(self, name):
         self.name = name
+        self.person = name
+        if name == 'You':
+            self.person += 'r'
         while True:
             try:
-                print(self.name, 'status (HP, ATK, DEF, EVD): ', end='')
+                print(self.person, 'status (HP, ATK, DEF, EVD): ', end='')
                 self.HP, self.ATK, self.DEF, self.EVD = map(int, input().split())
                 break
             except:
@@ -24,14 +27,15 @@ def main():
                 pass
 
 
-    def defensive(ATK, defender, action):
+    def def_result(ATK, defender, action):
+        '''return tuple(die_count, total_damage), total_damage counts only if the defender lives'''
 
         die_count, total_damage = 0, 0
         for roll in range(1, 7):
 
             if action == 'DEF':
             # try to defend
-                damage = max( 1 , ATK - (defender.DEF + roll) )
+                damage = max( 1 , ATK - max( 1, defender.DEF + roll) )
             elif action == 'EVD':
             # try to evade
                 damage = 0 if defender.EVD + roll > ATK else ATK
@@ -43,42 +47,81 @@ def main():
             
         return die_count, total_damage
 
-        
-    def offensive(attacker, defender):
-        
-        print('Offensive mode is working in progress.')
-        return
-        # WORKING IN PROGRESS
+
+    def defensive(ATK, defender):
+
+        for action in 'DEF', 'EVD':
+
+            die_count, total_damage = def_result(ATK, defender, action)
+
+            if die_count == 6:
+                print(f'[{action}] CERTAIN DEATH.')
+            elif die_count == 0:
+                print(f'[{action}] SURVIVE. Avarage damage: {total_damage / 6 :.2f}')
+            else:
+                print(f'[{action}] {die_count / 6 * 100:.2f}% Die;'
+                      f'      {(6-die_count) / 6 * 100:.2f}% Survive, avarage damage: {total_damage / (6-die_count):.2f}')
+
+
+
+    def offensive(attacker, defender, p = 1):
+    # p for possibility
+
+        if p == 0: return
+
+        die_count, total_damage = 0, 0
+        for roll in range(1, 7):
+
+            ATK = roll + attacker.ATK
+            def_die, def_dmg = def_result(ATK, defender, 'DEF')
+            evd_die, evd_dmg = def_result(ATK, defender, 'EVD')
+
+            caution_ratio = 0.8
+            # TODO what is the exact caution_bias?
+
+            if def_die * defender.HP + def_dmg > evd_die * defender.HP + evd_dmg + caution_ratio:
+            # suppose everyone is 'cautious', viz perfers to defend would they die less
+                die_count += evd_die / 6
+                total_damage += evd_dmg / 6
+            else:
+                die_count += def_die / 6
+                total_damage += def_dmg / 6
+
+        print(f'[{attacker.name} => {defender.name}]', '(whether happens or not)' if p < 1 else '')
+        if die_count == 6:
+            print('    CERTAIN DEATH.')
+            enemy.HP = 0
+        elif die_count == 0:
+            print(f'    SURVIVE. Avarage damage: {total_damage * p / (6-die_count):.2f}')
+            enemy.HP -= total_damage / (6-die_count)
+        else:
+            print(f'    {die_count * p / 6 * 100:.2f}% Slain.\n'
+                  f'    Avarage damage: {total_damage * p / (6-die_count):.2f}')
+            enemy.HP -= enemy.HP * die_count / 6 + total_damage * p / (6-die_count)
+
 
 
     while True:
-        print('\nChoose mode (Offensive(O/0) or Defensive(D/1)): ', end='')
+
+        print('\nChoose mode (Offend(O/0) or Defend(D/1)): ', end='')
         mode = input().lower()
 
-        if mode in ('d', '1', 'defensive', 'def'):
+        if mode in ('d', '1', 'defend', 'def'):
         # User is the defender
-            ATK = checked_input('Enemy attack: ', int)
-            you = Unit('Your')
-
-            for action in 'DEF', 'EVD':
-
-                die_count, total_damage = defensive(ATK, you, action)
-                if die_count == 6:
-                    print(f'[{action}] CERTAIN DEATH.')
-                else:
-                    print(f'[{action}] {die_count / 6 * 100:.2f}% Die;')
-                    print(' ' * 5, f'{(6-die_count) / 6 * 100:.2f}% Survive, \
-                        avarage damage: {total_damage / (6-die_count):.2f}.')
+            defensive(
+                checked_input('Enemy attack: ', int),
+                Unit('You')
+            )
 
 
-        elif mode in ('o', '0', 'offensive', 'off'):
+        elif mode in ('o', '0', 'offend', 'off'):
         # User is the offender
-            you = Unit('Your')        
+            you = Unit('You')        
             enemy = Unit('Enemy')
+            origin_enemy_HP = enemy.HP
 
             offensive(you, enemy)
-            if enemy.HP > 0:
-                offensive(enemy, you)
+            offensive(enemy, you, p = enemy.HP / origin_enemy_HP)
 
         else:
             print('Nigh!')
